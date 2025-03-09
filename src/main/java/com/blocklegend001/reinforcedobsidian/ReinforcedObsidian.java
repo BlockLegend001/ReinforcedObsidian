d3956efe91df60abd67c6b16223acc45f7ac04a4
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 14:37:19 +0100

    Aggiunta 1.20.1-Fabric

---
+++ src/main/java/com/blocklegend001/reinforcedobsidian/ReinforcedObsidian.java
@@ -0,0 +1,40 @@
+package com.blocklegend001.reinforcedobsidian;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ReinforcedObsidian implements ModInitializer {
+    public static final String MOD_ID = "reinforcedobsidian";
+
+    public static Block REINFORCED_OBSIDIAN = new Block(FabricBlockSettings.
+            copyOf(Blocks.OBSIDIAN).
+            requiresTool().
+            strength(50.0f, 3600000.0f));
+    public static BlockItem REINFORCED_OBSIDIAN_ITEM = new BlockItem(REINFORCED_OBSIDIAN, new FabricItemSettings());
+
+    public static final ItemGroup REINFORCED_OBSIDIAN_TAB = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(REINFORCED_OBSIDIAN_ITEM))
+            .displayName(Text.translatable("itemGroup.reinforcedobsidian"))
+            .entries((context, entries) -> {
+                entries.add(REINFORCED_OBSIDIAN_ITEM);
+            })
+            .build();
+
+    @Override
+    public void onInitialize() {
+        Registry.register(Registries.BLOCK, new Identifier(MOD_ID, "reinforced_obsidian_block"), REINFORCED_OBSIDIAN);
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID, "reinforced_obsidian_block"), REINFORCED_OBSIDIAN_ITEM);
+        Registry.register(Registries.ITEM_GROUP, new Identifier(MOD_ID, "reincorced_obsidian_tab"), REINFORCED_OBSIDIAN_TAB);
+    }
+}

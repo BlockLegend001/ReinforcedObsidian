7bda8fba30009b7e6bfdb8e9c2c7cec460eb8ce8
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 14:37:03 +0100

    Aggiunta 1.21.1-Forge

---
+++ src/main/java/com/blocklegend001/reinforcedobsidian/datagen/DataGenerators.java
@@ -0,0 +1,30 @@
+package com.blocklegend001.reinforcedobsidian.datagen;
+
+import com.blocklegend001.reinforcedobsidian.ReinforcedObsidian;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.concurrent.CompletableFuture;
+
+@Mod.EventBusSubscriber(modid = ReinforcedObsidian.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput, lookupProvider));
+        generator.addProvider(event.includeServer(), ModLootTableProvider.create(packOutput, lookupProvider));
+
+        BlockTagsProvider blockTagsProvider = new ModBlockTagGenerator(packOutput, lookupProvider, existingFileHelper);
+        generator.addProvider(event.includeServer(), blockTagsProvider);
+    }
+}

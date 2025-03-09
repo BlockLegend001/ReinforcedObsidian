5b9a1a8e19d1d54e5d9a95efc2bfaff3077bad79
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 14:37:07 +0100

    Aggiunta 1.21.3-Forge

---
+++ src/main/java/com/blocklegend001/reinforcedobsidian/datagen/ModBlockTagGenerator.java
@@ -0,0 +1,31 @@
+package com.blocklegend001.reinforcedobsidian.datagen;
+
+import com.blocklegend001.reinforcedobsidian.ReinforcedObsidian;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+import static com.blocklegend001.reinforcedobsidian.ReinforcedObsidian.REINFORCED_OBSIDIAN_BLOCK;
+
+public class ModBlockTagGenerator extends BlockTagsProvider {
+    public ModBlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, ReinforcedObsidian.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(REINFORCED_OBSIDIAN_BLOCK.get());
+
+        this.tag(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(REINFORCED_OBSIDIAN_BLOCK.get());
+
+        this.tag(BlockTags.WITHER_IMMUNE)
+                .add(REINFORCED_OBSIDIAN_BLOCK.get());
+    }
+}

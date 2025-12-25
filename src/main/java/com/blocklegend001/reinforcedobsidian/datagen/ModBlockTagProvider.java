222f71f87b6d4f48965935274b1363e5ccaf53e6
BlockLegend001 <rosarioarcamone@gmail.com>
2025-12-25 22:45:09 +0100

    Added 1.21.11-Fabric

---
+++ src/main/java/com/blocklegend001/reinforcedobsidian/datagen/ModBlockTagProvider.java
@@ -0,0 +1,29 @@
+package com.blocklegend001.reinforcedobsidian.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
+
+import java.util.concurrent.CompletableFuture;
+
+import static com.blocklegend001.reinforcedobsidian.ReinforcedObsidian.REINFORCED_OBSIDIAN;
+
+public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
+    public ModBlockTagProvider(FabricDataOutput fabricDataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(fabricDataOutput, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        valueLookupBuilder(BlockTags.PICKAXE_MINEABLE)
+                .add(REINFORCED_OBSIDIAN);
+
+        valueLookupBuilder(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(REINFORCED_OBSIDIAN);
+
+        valueLookupBuilder(BlockTags.WITHER_IMMUNE)
+                .add(REINFORCED_OBSIDIAN);
+    }
+
+}
